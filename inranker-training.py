683a84141b81214076ba057b4ccb7d5ba455af73
Pratyaksh Sharma <pratyaksh@me.com>
2024-07-22 15:22:05 -0700

    wip

--- inranker-training.py
+++ inranker-training.py
@@ -1,4 +1,5 @@
 import mlflow
+import json
 
 mlflow.set_experiment("/Users/praty@dashworks.ai/inranker-ft-dashworks-queries")
 from inranker import InRankerTrainer
@@ -32,4 +33,23 @@ split_datasets = dataset_dict["train"].train_test_split(test_size=validation_siz
 train_dataset = split_datasets["train"]
 validation_dataset = split_datasets["test"]
 
-trainer.train(train_dataset=train_dataset, validation_dataset=validation_dataset)
+def write_dataset_to_jsonl(dataset, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        for item in dataset:
+            # Reconstruct the original format
+            output = {
+                "query": item['query'],
+                "contents": item['text'],
+                "true_logit": item['label'][1],
+                "false_logit": item['label'][0]
+            }
+            json.dump(output, f)
+            f.write('\n')
+
+# Write train dataset
+write_dataset_to_jsonl(train_dataset, "inranker_training_data_jul18_train.jsonl")
+
+# Write validation dataset
+write_dataset_to_jsonl(validation_dataset, "inranker_training_data_jul18_validation.jsonl")
+
+# trainer.train(train_dataset=train_dataset, validation_dataset=validation_dataset)
